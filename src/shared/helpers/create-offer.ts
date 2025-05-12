5afc48ffd6fa6cca31f5a16c2e70636f9f7e393a
VladimirLezhnin <115662980+VladimirLezhnin@users.noreply.github.com>
2025-05-12 22:01:53 +0500

    task done

---
+++ src/shared/helpers/create-offer.ts
@@ -0,0 +1,60 @@
+import { Cities } from '../../mocks/cities.js';
+import { HousingType } from '../types/housing-type.enum.js';
+import { Amenity, RentalOffer } from '../types/offer.type.js';
+import { UserType } from '../types/user-type.enum.js';
+import { parseBoolean } from './common.js';
+
+export function createOffer(offerData : string) : RentalOffer {
+  function findOrThrow<T>(array: T[], predicate: (item: T) => boolean, errorMessage: string): T {
+    const result = array.find(predicate);
+    if (result === undefined) {
+      throw new Error(errorMessage);
+    }
+    return result;
+  }
+
+  const [
+    title,
+    description,
+    cityName,
+    createdDate,
+    previewImageUrl,
+    imagesUrls,
+    isFavorite,
+    isPremium,
+    housingType,
+    rating,
+    roomsCount,
+    guestsCount,
+    rentalCost,
+    amenitiesString,
+    ownerInfo,
+    commentsCount,
+    coordinates] = offerData.split('\t');
+
+  return {
+    title,
+    description,
+    city: findOrThrow(Cities, (city) => city.name === cityName, 'Unknown city!'),
+    postDate: new Date(createdDate),
+    previewImage: previewImageUrl,
+    images: imagesUrls.split(';'),
+    isFavorite: parseBoolean(isFavorite),
+    isPremium: parseBoolean(isPremium),
+    housingType: housingType as HousingType,
+    rating: Number.parseInt(rating, 10),
+    roomsCount: Number.parseInt(roomsCount, 10),
+    guestsCount: Number.parseInt(guestsCount, 10),
+    rentalCost: Number.parseInt(rentalCost, 10),
+    amenities: amenitiesString.split(';') as Amenity[],
+    owner: (() => {
+      const [name, email, avatarPath, userType] = ownerInfo.split(';');
+      return { name, email, avatarPath, userType: userType as UserType };
+    })(),
+    commentsCount: Number.parseInt(commentsCount, 10),
+    coordinates: (() => {
+      const [latitude, longitude] = coordinates.split(';');
+      return { latitude: Number.parseInt(latitude, 10), longitude: Number.parseInt(longitude, 10) };
+    })()
+  };
+}
---
+++ src/shared/types/mock-server-data.ts
@@ -0,0 +1,13 @@
+export type MockServerData = {
+    titles: string[];
+    descriptions: string[];
+    postDates: string[];
+    previewImages: string[];
+    images: string[];
+    housingTypes: string[];
+    amenities: string[];
+    userNames: string[];
+    emails: string[];
+    avatars: string[];
+    userTypes: string[];
+};
